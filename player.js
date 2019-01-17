62703a8918a8ea9e191908d64280b559100170b0
1asalinas <42749574+1asalinas@users.noreply.github.com>
2019-01-17 13:57:02 -0600

    Add files via upload

--- player.js
+++ player.js
@@ -20,16 +20,78 @@ let p2Stats = {
   sp: 100
 }
 
-let battle = {
 
-  attack: function(){},
-  defend: function(){}
+//not finished
+let p1battle = {
+
+  attack: function(mv){
+    if (mv == 1){
+      return this.moves.punch();
+    }
+
+    else if(mv == 2){
+      return this.moves.other();
+    }
+  },
+  defend: function(atkmv){
+    let rawDamage = atkmv - this.stats.def;
+
+    this.stats.hp = this.stats.hp - rawDamage;
+
+    if(this.stats.hp < 0){
+      this.stats.hp = 0;
+    }
+  }
 
 }
 
+let physical = {
+  razorPunch: function(){
+    let baseDamage = this.stats.atk * 0.75;
+    let bonus = 0.5 * this.stats.atk * Math.random();
+    return baseDamage + bonus;
+  },
+  charge: function(){
+    let pain = Math.random();
+    let bonus = 0.5 * this.stats.atk * Math.random();
+    if(pain < 0.50){
+      this.stats.hp = this.stats.hp - this.stats.atk / 4;
+      return this.stats.atk + bonus;
+    }
+    else{
+      return this.stats.atk + bonus;
+    }
+  }
+}
 
+let magic = {
+  hydroPump: function(){
+    if(this.stats.mp >= 8){
+      this.stats.mp = this.stats.mp - 8;
+      let tetraDice = Math.random();
+      let pump = Math.ceil(this.stats.sp / 3);
 
+      if(tetraDice < 0.10){
+        return pump;
+      }
 
+      else if(tetraDice < 0.30){
+        return 2 * pump;
+      }
+
+      else if(tetraDice < 0.60){
+        return 3 * pump;
+      }
+
+      else{
+        return 4 * pump;
+      }
+
+    }
+
+  }
+  
+}
 
 
 
