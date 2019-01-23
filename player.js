aed59acb43f1bf1e1c5ab7abf4dd0e872865686a
1asalinas <42749574+1asalinas@users.noreply.github.com>
2019-01-23 14:02:22 -0600

    Add files via upload

--- player.js
+++ player.js
@@ -3,6 +3,7 @@
 let p1Name = "";
 
 let p1Stats = {
+
   hp: 200,
   mp: 0,
   atk: 100,
@@ -10,19 +11,54 @@ let p1Stats = {
   sp: 0
 }
 
-let p2Name = "";
+let p1Moves = {
+  RazorPunch: function(){
 
-let p2Stats = {
-  hp: 75,
-  mp: 125,
-  atk: 0,
-  def: 20,
-  sp: 100
+    let baseDamage = this.stats.atk * 0.75;
+    let bonus = 0.5 * this.stats.atk * Math.random();
+    return baseDamage + bonus;
+  },
+
+  FalconPunch: function(){
+
+    let baseDamage = this.stats.atk * 0.50;
+    let bonus = this.stats.atk * Math.random();
+    return baseDamage + bonus;
+  },
+
+  Charge: function(){
+
+    let pain = Math.random();
+    let bonus = 0.5 * this.stats.atk * Math.random();
+    if(pain < 0.50){
+      this.stats.hp = this.stats.hp - this.stats.atk / 4;
+      return this.stats.atk + bonus;
+    }
+    else{
+      return this.stats.atk + bonus;
+    }
+  },
+
+  Roundhouse_Kick: function(){
+
+    let tetraDice = Math.random();
+    let kick = Math.ceil(this.stats.atk / 2);
+
+    if(tetraDice < 0.33){
+      return kick;
+    }
+
+    else if(tetraDice < 0.66){
+      return 2 * kick;
+    }
+
+    else{
+     return 3 * kick;
+    }
+  }
 }
 
-
-//not finished
-let p1battle = {
+let p1Battle = {
 
   attack: function(mv){
     if (mv == 1){
@@ -33,6 +69,7 @@ let p1battle = {
       return this.moves.other();
     }
   },
+
   defend: function(atkmv){
     let rawDamage = atkmv - this.stats.def;
 
@@ -44,6 +81,199 @@ let p1battle = {
   }
 
 }
+
+let player1 = {
+  name: p1Name,
+  stats: p1Stats,
+  moves: p1Moves,
+  battle: p1Battle
+}
+
+
+
+
+
+
+let p2Name = "";
+
+let p2Stats = {
+
+  hp: 75,
+  mp: 125,
+  atk: 0,
+  def: 20,
+  sp: 100
+}
+
+let p2Moves = {
+  Pyro_ball: function(){
+
+    if(this.stats.mp >= 6){
+
+      this.stats.mp = this.stats.mp - 6;
+      let baseDamage = this.stats.atk * 0.88;
+      let bonus = 0.32 * this.stats.atk * Math.random();
+      return baseDamage + bonus;
+    }
+  },
+
+  hydroPump: function(){
+
+    if(this.stats.mp >= 8){
+
+      this.stats.mp = this.stats.mp - 8;
+      let tetraDice = Math.random();
+      let pump = Math.ceil(this.stats.sp / 3);
+
+      if(tetraDice < 0.10){
+        return pump;
+      }
+
+      else if(tetraDice < 0.30){
+        return 2 * pump;
+      }
+
+      else if(tetraDice < 0.60){
+        return 3 * pump;
+      }
+
+      else{
+        return 4 * pump;
+      }
+
+    }
+
+  },
+
+  Blizzard: function(){
+
+    if(this.stats.mp >= 14){
+      this.stats.mp = this.stats.mp - 14;
+      let baseDamage = this.stats.atk * 0.50;
+      let bonus = this.stats.atk * Math.random();
+      return baseDamage + bonus;
+    }
+  },
+
+  hyperBeam: function(){
+
+    if(this.stats.mp >= 20){
+      this.stats.mp = this.stats.mp - 20;
+    let damage = this.stats.sp * 1.50;
+    return damage;
+    }
+
+  }
+  
+}
+
+let p2Battle = {
+
+  attack: function(mv){
+    if (mv == 1){
+      return this.moves.punch();
+    }
+
+    else if(mv == 2){
+      return this.moves.other();
+    }
+  },
+
+  defend: function(atkmv){
+
+    let rawDamage = atkmv - this.stats.def;
+
+    this.stats.hp = this.stats.hp - rawDamage;
+
+    if(this.stats.hp < 0){
+      this.stats.hp = 0;
+    }
+  }
+
+}
+
+let player2 = {
+  name: p2Name,
+  stats: p2Stats,
+  moves: p2Moves,
+  battle: p2Battle
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//not finished
+
 
 let physical = {
 
